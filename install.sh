de746a7e9ea1548687c3def8f58be01b826b8bd7
bobvanluijt <bob@kubrickolo.gy>
2019-05-22 20:47:53 +0200

    gh-xxx: update install sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 CURRENTDIR=$(pwd)
 cd ~
 git clone https://github.com/semi-technologies/weaviate-cli
-echo ~/weaviate-cli/bin >> ~/.profile
+echo export $PATH=$PATH:~/weaviate-cli/bin >> ~/.profile
 cd $CURRENTDIR
 clear
-echo "Weaviate-cli installation is done - open a new CLI and run weaviate-cli --version"
+echo "Weaviate-cli installation is done - run weaviate-cli --version" &
