9738cf6e50afb914e1ca3f7cc4e00a8d18390ec9
bobvanluijt <bob@kubrickolo.gy>
2019-05-22 21:17:40 +0200

    gh-xxx: Add to bash profile

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ CURRENTDIR=$(pwd)
 cd ~
 git clone https://github.com/semi-technologies/weaviate-cli
 echo 'export $PATH=$PATH:~/weaviate-cli/bin' >> ~/.profile
+echo 'export $PATH=$PATH:~/weaviate-cli/bin' >> ~/.bash_profile
 cd $CURRENTDIR
 clear
 echo "Weaviate-cli installation is done - run weaviate-cli --version" &
