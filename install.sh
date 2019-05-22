3adfdb23f4951d33dac1ebd6f4166703002e9150
bobvanluijt <bob@kubrickolo.gy>
2019-05-22 21:33:05 +0200

    gh-xxx: Set export correctly

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 CURRENTDIR=$(pwd)
 cd ~
 git clone https://github.com/semi-technologies/weaviate-cli
-echo 'export $PATH=$PATH:'$(pwd)'/weaviate-cli/bin' >> ~/.profile
-echo 'export $PATH=$PATH:'$(pwd)'/weaviate-cli/bin' >> ~/.bash_profile
+echo 'export PATH=$PATH:'$(pwd)'/weaviate-cli/bin' >> ~/.profile
+echo 'export PATH=$PATH:'$(pwd)'/weaviate-cli/bin' >> ~/.bash_profile
 cd $CURRENTDIR
 clear
 echo "Weaviate-cli installation is done."
