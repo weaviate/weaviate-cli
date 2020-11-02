3a63decfe584c16bdc13b5d69ca05b7dea3ec1ca
Felix Finkbeiner <4573211+fefi42@users.noreply.github.com>
2020-11-02 10:53:34 +0100

    Add schema import and export

--- cli.py
+++ cli.py
@@ -1,6 +1,6 @@
 import click
 from semi.config.configuration import Configuration
-
+import json
 
 @click.group()
 @click.pass_context
@@ -34,13 +34,25 @@ def main_ping():
 
 
 # schema
+
+
 @schema_group.command("import")
-def schema_import():
-    click.echo("TODO impl")
+@click.pass_context
+@click.argument('filename')
+def schema_import(ctx, filename):
+    print("Importing file: ", filename)
+    cfg = _get_config_from_context(ctx)
+    cfg.client.schema.create(filename)
 
 @schema_group.command("export")
-def schema_export():
-    click.echo("TODO impl")
+@click.pass_context
+@click.argument('filename')
+def schema_export(ctx, filename):
+    print("Exporting to file: ", filename)
+    cfg = _get_config_from_context(ctx)
+    schema = cfg.client.schema.get()
+    with open(filename, 'w') as output_file:
+        json.dump(schema, output_file, indent=4)
 
 @schema_group.command("truncate")
 def schema_truncate():
@@ -75,6 +87,15 @@ def cloud_create():
 def cloud_delete():
     click.echo("TODO impl")
 
+
+def _get_config_from_context(ctx):
+    """
+
+    :param ctx:
+    :return:
+    :rtype: semi.config.configuration.Configuration
+    """
+    return ctx.obj["config"]
 
 if __name__ == "__main__":
     main()
