db034458731bfe3b09df99438eef83126cbca393
Felix Finkbeiner <4573211+fefi42@users.noreply.github.com>
2020-11-03 10:03:48 +0100

    Fix typo in test script

--- test/create_test_config.bash
+++ test/create_test_config.bash
@@ -2,6 +2,6 @@
 
 set -e
 
-mkdir -p $HOME.config/semi_technologies/
+mkdir -p $HOME/.config/semi_technologies/
 echo '{"url": "http://localhost:8080", "auth": null}' > "$HOME.config/semi_technologies/configs.json"
 
