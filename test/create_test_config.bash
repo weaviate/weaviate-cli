fe749c70cf9b4c8f4ad6c6eb3d4b033009aba315
Felix Finkbeiner <4573211+fefi42@users.noreply.github.com>
2020-11-03 10:08:51 +0100

    Debug travis test

--- test/create_test_config.bash
+++ test/create_test_config.bash
@@ -2,7 +2,7 @@
 
 set -e
 
-echo "$HOME/.config/semi_technologies/"
-mkdir -p "$HOME/.config/semi_technologies/"
+NEW_DIR="$HOME/.config/semi_technologies/"
+mkdir -p "$NEW_DIR"
 echo '{"url": "http://localhost:8080", "auth": null}' > "$HOME.config/semi_technologies/configs.json"
 
