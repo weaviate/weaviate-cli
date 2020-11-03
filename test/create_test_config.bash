39d270b212b1b589efe59c07aa792251ffef5cfa
Felix Finkbeiner <4573211+fefi42@users.noreply.github.com>
2020-11-03 10:14:34 +0100

    Fix right command

--- test/create_test_config.bash
+++ test/create_test_config.bash
@@ -2,7 +2,6 @@
 
 set -e
 
-NEW_DIR="$HOME/.config/semi_technologies/"
-mkdir -p "$NEW_DIR"
-echo '{"url": "http://localhost:8080", "auth": null}' > "$HOME.config/semi_technologies/configs.json"
+mkdir -p "$HOME/.config/semi_technologies/"
+echo '{"url": "http://localhost:8080", "auth": null}' > "$HOME/.config/semi_technologies/configs.json"
 
