41fdded99bdc41ea0b5d7338dc66e99efa5c565c
Felix Finkbeiner <4573211+fefi42@users.noreply.github.com>
2020-11-03 10:06:51 +0100

    Debug travis test

--- test/create_test_config.bash
+++ test/create_test_config.bash
@@ -2,6 +2,7 @@
 
 set -e
 
-mkdir -p $HOME/.config/semi_technologies/
+echo "$HOME/.config/semi_technologies/"
+mkdir -p "$HOME/.config/semi_technologies/"
 echo '{"url": "http://localhost:8080", "auth": null}' > "$HOME.config/semi_technologies/configs.json"
 
