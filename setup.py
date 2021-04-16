7df76c64ea5186947bfb55f00bb650be618d1a50
Stefan Bogdan <stefanbogdan180394@gmail.com>
2021-04-16 16:28:23 +0200

    version 2.1.1 with fixed PyPI long description links

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name="weaviate-cli",
-      version="2.1.1rc0", # 0.1.0rc0
+      version="2.1.1", # 0.1.0rc0
       description="Comand line interface to interact with weaviate",
       long_description=long_description,
       long_description_content_type='text/x-rst',
